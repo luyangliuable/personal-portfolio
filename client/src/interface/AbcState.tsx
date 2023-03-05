db07ae7b77fa28a435bf12e181b107c0729056cd
Luyang Liu <luyangl16@gmail.com>
2023-03-05 13:27:49 +1100

    Added fonts and redux for blog.

--- client/src/interface/AbcState.tsx
+++ client/src/interface/AbcState.tsx
@@ -1,7 +1,5 @@
 export default interface AbcState {
-    navBarHeight: number,
     render: () => React.ReactElement<any, any>,
     componentDidUpdate: () => void,
     componentDidMount: () => void,
-    /* state: {scrolled: any} */ // TODO fix
 };
