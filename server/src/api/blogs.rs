b67e1fcd6dce6a9f0200351c0be0c3897d277c76
Luyang Liu <luyangl16@gmail.com>
2023-07-30 20:36:38 +1000

    Updated backend to include date created and author of blogposts.

--- server/src/api/blogs.rs
+++ server/src/api/blogs.rs
@@ -3,12 +3,15 @@ use mongodb::bson::{oid::ObjectId};
 use rocket::{http::Status, State};
 use crate::{models::blog_model::BlogPost, repository::mongodb_repo::MongoRepo};
 use mongodb::results::InsertOneResult;
+use chrono::{DateTime, Utc};
 
 #[get("/blogs/test")]
 pub fn get_blog_posts() -> Json<BlogPost> {
     let blog_post = BlogPost {
         id: Some(ObjectId::new()),
+        author: String::from("John Doe"),
         heading: "Hello World".to_string(),
+        date_created: Utc::now().format("%Y-%m-%d %H:%M:%S").to_string(),
         body: vec!["This is a blog post".to_string()]
     };
 
@@ -23,9 +26,10 @@ pub fn create_blog(
     let data = BlogPost {
         id: None,
         heading: new_blog.heading.to_owned(),
+        date_created: new_blog.date_created.to_owned(),
+        author: new_blog.author.to_owned(),
         body: new_blog.body.to_owned(),
     };
-
     let user_detail = db.create_blog(data);
 
     match user_detail {
