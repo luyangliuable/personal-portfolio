da6f1038c29bc824d0628435339b57533d6cbe19
Restyled.io <commits@restyled.io>
2023-12-19 11:28:21 +0000

    Restyled by prettier

--- client/.babelrc.js
+++ client/.babelrc.js
@@ -1,24 +1,17 @@
-const env = require('./env-config')
+const env = require("./env-config");
 
 module.exports = {
-  presets : [ 'next/babel' ],
-  plugins :
-          [
-            [
-              'transform-define',
-              env,
-            ],
-            [
-              'prismjs',
-              {
-                'languages' : [ 'shell', 'javascript', 'css', 'html', 'jsx' ],
-                'plugins' :
-                          [
-                            'line-numbers', 'show-language', 'copy-to-clipboard'
-                          ],
-                'theme' : 'tomorrow',
-                'css' : false
-              },
-            ]
-          ],
-}
+  presets: ["next/babel"],
+  plugins: [
+    ["transform-define", env],
+    [
+      "prismjs",
+      {
+        languages: ["shell", "javascript", "css", "html", "jsx"],
+        plugins: ["line-numbers", "show-language", "copy-to-clipboard"],
+        theme: "tomorrow",
+        css: false,
+      },
+    ],
+  ],
+};
