0b20b92c4e8f9b753d2c9ed2c7386556824d6eab
Restyled.io <commits@restyled.io>
2023-12-19 11:28:17 +0000

    Restyled by clang-format

--- client/.babelrc.js
+++ client/.babelrc.js
@@ -1,19 +1,24 @@
 const env = require('./env-config')
 
 module.exports = {
-  presets: ['next/babel'],
-  plugins: [
-    [
-      'transform-define',
-      env,
-    ],
-    [
-      'prismjs', {
-	'languages': ['shell','javascript', 'css', 'html', 'jsx'],
-        'plugins': ['line-numbers', 'show-language', 'copy-to-clipboard'],
-        'theme': 'tomorrow',
-        'css': false
-      },
-    ]
-  ],
+  presets : [ 'next/babel' ],
+  plugins :
+          [
+            [
+              'transform-define',
+              env,
+            ],
+            [
+              'prismjs',
+              {
+                'languages' : [ 'shell', 'javascript', 'css', 'html', 'jsx' ],
+                'plugins' :
+                          [
+                            'line-numbers', 'show-language', 'copy-to-clipboard'
+                          ],
+                'theme' : 'tomorrow',
+                'css' : false
+              },
+            ]
+          ],
 }
