49cfd5306cd70b8f8627efa66a1ddb9621104d7b
Luyang Liu <luyang.l@proton.me>
2024-01-07 11:37:19 +1100

    Resume page to use own served files from backend instead of from gdrive.

--- client/src/pages/ResumePage/Interface/IResumePageState.tsx
+++
@@ -1,4 +0,0 @@
-interface IResumePageState {
-}
-
-export default IResumePageState;
--- client/src/pages/ResumePage/ResumePage.tsx
+++ client/src/pages/ResumePage/ResumePage.tsx
@@ -1,29 +1,14 @@
-import React, { Component } from 'react';
-import IResumePageState from "./Interface/IResumePageState";
+import React from 'react';
 import IResumePageProps from "./Interface/IResumePageProps";
 import "./ResumePage.css";
 
-class ResumePage extends Component<IResumePageProps, IResumePageState> {
-    constructor(props: IResumePageProps) {
-        super(props);
-        this.state = {};
-    }
-
-
-    componentDidUpdate(): void {
-    }
-
-    componentDidMount(): void {
-    }
-
-    render(): React.ReactElement<any, any> {
-        return (
-            <div className="resume-page-content">
-                <iframe className="browser" src="http://docs.google.com/document/d/18WT-J7ZP5dcEJreXIvldSm1VySQhC0DQB0GzBXGyJEQ/edit" title="Resume">
-                </iframe>
-            </div>
-        )
-    }
+const ResumePage: React.FC<IResumePageProps> = (props) => {
+    return (
+        <div className="resume-page-content">
+            <iframe className="browser" src="http://llcode.tech/api/image/6599eebc58701a6b8fe5908a" title="Resume">
+            </iframe>
+        </div>
+    )
 }
 
 export default ResumePage;
